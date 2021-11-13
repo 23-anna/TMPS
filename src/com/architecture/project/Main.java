f37e6462b979d0cfdc41187e9a848df858ddd670
23-anna <55646413+23-anna@users.noreply.github.com>
2021-11-13 22:26:08 +0200

    Add files via upload

--- src/com/architecture/project/Main.java
+++ src/com/architecture/project/Main.java
@@ -1,5 +1,8 @@
 package com.architecture.project;
 
+import com.architecture.project.bankOperations.Card;
+import com.architecture.project.bankOperations.Cash;
+import com.architecture.project.bankOperations.IMoney;
 import com.architecture.project.builders.BusinessCenterBuilder;
 import com.architecture.project.builders.HouseBuilder;
 import com.architecture.project.builders.SchoolBuilder;
@@ -8,6 +11,7 @@ import com.architecture.project.buildings.House;
 import com.architecture.project.buildings.School;
 import com.architecture.project.design.Exterior;
 import com.architecture.project.design.Interior;
+import com.architecture.project.design.InteriorAttributes;
 
 import java.util.Scanner;
 
@@ -42,8 +46,7 @@ public class Main {
                     System.out.println("Do you want some personal designs?");
                     String schoolAnswer = scanner.nextLine();
                     if (schoolAnswer.equals("Yes")){
-                        Designer designer = new Interior();
-                        designer.startDesign();
+                        settingsAndPayment(scanner);
                     }
                     break;
 
@@ -59,8 +62,7 @@ public class Main {
                     System.out.println("Do you want some personal designs?");
                     String businessAnswer = scanner.nextLine();
                     if (businessAnswer.equals("Yes")){
-                        Designer designer = new Interior();
-                        designer.startDesign();
+                        settingsAndPayment(scanner);
                     }
                     break;
 
@@ -77,8 +79,7 @@ public class Main {
                     System.out.println("Do you want some personal designs?");
                     String houseAnswer = scanner.nextLine();
                     if (houseAnswer.equals("Yes")){
-                        Designer designer = new Interior();
-                        designer.startDesign();
+                        settingsAndPayment(scanner);
                     }
                     break;
 
@@ -96,8 +97,54 @@ public class Main {
 
                 System.out.println("Your project is ready!");
             }
+
+
         } catch (Exception e){
             System.out.println("Check word spelling or the correctness of data");
+        }
+    }
+
+    static void settingsAndPayment(Scanner scanner){
+        String color = "";
+        String material = "";
+        InteriorAttributes objects = new InteriorAttributes();
+
+        System.out.println("Table material:");
+        material = scanner.nextLine();
+        System.out.println("Table color:");
+        color = scanner.nextLine();
+        objects.setTable(color, material);
+
+        System.out.println("Mirror material:");
+        material = scanner.nextLine();
+        System.out.println("Mirror color:");
+        color = scanner.nextLine();
+        objects.setMirror(color, material);
+
+        System.out.println("Curtain material:");
+        material = scanner.nextLine();
+        System.out.println("Curtain color:");
+        color = scanner.nextLine();
+        objects.setCurtain(color, material);
+
+        System.out.println("Armchair material:");
+        material = scanner.nextLine();
+        System.out.println("Armchair color:");
+        color = scanner.nextLine();
+        objects.setArmchair(color, material);
+
+        Designer designer = new Interior();
+        designer.startDesign();
+
+        int totalPrice = objects.getTotalPrice();
+        System.out.println("Design's total price: " + totalPrice);
+
+        Cash cash = new Cash();
+        Card card = new Card(cash);
+        System.out.println("Do you want to pay by card?");
+        String answer = scanner.nextLine();
+        if (answer.equals("Yes")){
+            card.doTransaction(totalPrice);
         }
     }
 }
